1e8df29eb7b30a3eaf62e5035d55462f428af702
rampfire <abishek.ur@gmail.com>
2021-06-18 08:43:33 +0530

    norm type parameter modified

--- Session6/scripts/model.py
+++ Session6/scripts/model.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-def normalization_technique(normalization,in_channels,groups = None):
+def normalization_technique(normalization,in_channels):
     if normalization == "GN":
-        return nn.GroupNorm(groups,in_channels)
+        return nn.GroupNorm(2,in_channels)
     elif normalization == "LN":
         return nn.GroupNorm(1,in_channels)
     else:
@@ -15,21 +15,21 @@ class Net(nn.Module):
         self.convblock1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3),
             nn.ReLU(),
-            normalization_technique("BN",8),
+            normalization_technique(norm_type,8),
             nn.Dropout(0.01)
         )
         #28/26/3
         self.convblock2 = nn.Sequential(
             nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(8),
+            normalization_technique(norm_type,8),
             nn.Dropout(0.01)
         )
         #26/24/5
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
+            normalization_technique(norm_type,16),
             nn.Dropout(0.01)
         )
         #24/22/7
@@ -37,7 +37,7 @@ class Net(nn.Module):
         self.convblock4 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=18, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(18),
+            normalization_technique(norm_type,18),
             nn.Dropout(0.01)
         )
         #22/20/9
@@ -46,21 +46,21 @@ class Net(nn.Module):
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=18, out_channels=14, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(14),
+            normalization_technique(norm_type,14),
             nn.Dropout(0.01)
         )
         #10/8/14
         self.convblock6 = nn.Sequential(
             nn.Conv2d(in_channels=14, out_channels=14, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(14),
+            normalization_technique(norm_type,14),
             nn.Dropout(0.01)
         )
         #8/6/18
         self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=14, out_channels=10, kernel_size=1),
             nn.ReLU(),
-            nn.BatchNorm2d(10),
+            normalization_technique(norm_type,10),
             nn.Dropout(0.01)
         )
         #6/6/18
