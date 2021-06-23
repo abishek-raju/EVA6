1a498006cccd75ae0a3d410db3e6df166506f84c
rampfire <abishek.ur@gmail.com>
2021-06-23 06:28:54 +0530

    model file modified to take 3 channels

--- Session7/scripts/dataset.py
+++ Session7/scripts/dataset.py
@@ -1,14 +1,40 @@
 from torchvision import datasets, transforms
 
 
-def train_dataset(train_transform, root_dir = '../data'):
-
-    tr_dataset = datasets.MNIST(root_dir, train=True, download=True,
-                   transform = train_transform)
+def train_dataset(train_transform, dataset : str, 
+                  root_dir : str  = '../data')->"dataset_obj":
+    """
+    Function which returns the dataset object according to the dataset
+    parameter.
+    
+    Params:
+        train_transform : Transform's compose object.
+        dataset : MNIST/CIFAR10.
+        root_dir : path to store data.
+    """
+    if dataset == "MNIST":
+        tr_dataset = datasets.MNIST(root_dir, train=True, download=True,
+                       transform = train_transform)
+    elif dataset == "CIFAR10":
+        tr_dataset = datasets.CIFAR10(root_dir, train=True,
+                                        download=True, train_transform)
     return tr_dataset
 
-def test_dataset(test_transform, root_dir = '../data'):
-
-    ts_dataset = datasets.MNIST(root_dir, train=False, download=True,
-                   transform = test_transform)
+def test_dataset(test_transform, dataset : str, 
+                  root_dir : str  = '../data')->"dataset_obj":
+    """
+    Function which returns the dataset object according to the dataset
+    parameter.
+    
+    Params:
+        test_transform : Transform's compose object.
+        dataset : MNIST/CIFAR10.
+        root_dir : path to store data.
+    """
+    if dataset == "MNIST":
+        ts_dataset = datasets.MNIST(root_dir, train=False, download=True,
+                       transform = test_transform)
+    elif dataset == "CIFAR10":
+        ts_dataset = datasets.CIFAR10(root_dir, train=False,
+                                        download=True, test_transform)
     return ts_dataset
