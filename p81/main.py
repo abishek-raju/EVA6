e9a54c8de9ef23fe3d9ccf99a3bc13e51f2a9c59
rampfire <abishek.ur@gmail.com>
2021-07-01 20:05:53 +0530

    modified main.py

--- p81/main.py
+++ p81/main.py
@@ -17,8 +17,8 @@ import argparse
 
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-model', '--model', required = True,default=False) 
-    parsed = parser.parse_args()
-    print(vars(parsed))
+#if __name__ == "__main__":
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('-model', '--model', required = True,default=False) 
+#    parsed = parser.parse_args()
+#    print(vars(parsed))
