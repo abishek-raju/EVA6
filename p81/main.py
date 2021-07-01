011e09a863b6d2b7d6696ce87b5727e397d039b7
rampfire <abishek.ur@gmail.com>
2021-07-01 21:05:21 +0530

    modified main.py

--- p81/main.py
+++ p81/main.py
@@ -9,9 +9,11 @@ Created on Thu Jul  1 19:49:24 2021
 
 from .data import train_dataloader_obj,test_dataloader_obj
 from .models import resnet
+from .training import training,testing
 
-
-
+from torch.optim.lr_scheduler import StepLR
+import torch.optim as optim
+import torch.nn.functional as F
 
 
 def main(config_json):
@@ -24,4 +26,47 @@ def main(config_json):
                                       dataloader_kwargs = config_json["dev_kwargs"])
     
     net = resnet.ResNet18()
-    print(net)
+#    print(net)
+    model = net.to(config_json["device"])
+    
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay = lambda_l2)
+    scheduler = StepLR(optimizer, step_size=70, gamma=0.15)
+    train_loss = []
+    test_loss = []
+    
+    train_accuracy = []
+    test_accuracy = []
+    for epoch in range(1, 100):
+
+    tr_loss,tr_acc = train(model, device, train_loader, F.nll_loss, optimizer, epoch, lambda_l1)
+    scheduler.step()
+    tst_loss,tst_acc = test(model, device, test_loader, epoch, F.nll_loss, lambda_l1)
+    train_loss.append(tr_loss),train_accuracy.append(tr_acc)
+    test_loss.append(tst_loss),test_accuracy.append(tst_acc)
+    print("Train_epoch : ",100*tr_acc.item())
+    print("Test_epoch : ",100*tst_acc.item())
+    print("Learning Rate : ",optimizer.param_groups[0]['lr'])
+#    writer.add_scalars('Loss',
+#    {
+#    'train_loss': tr_loss,
+#    'test_loss': tst_loss,
+#    },epoch)
+#    writer.add_scalars('Accuracy',
+#    {
+#    'train_accuracy': tr_acc,
+#    'test_accuracy': tst_acc,
+#    },epoch)
+#    writer.flush()
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ p81/training/testing.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import sys
+IN_COLAB = 'google.colab' in sys.modules
+if IN_COLAB:
+    from tqdm.notebook import tqdm
+else:
+    import tqdm
+def test(model, device, test_loader, epoch, loss_function, lambda_l1 = None):
+    average_epoch_loss = 0
+    correct_predictions_epoch = 0
+    model.eval()
+    with torch.no_grad():
+        with tqdm(test_loader, unit="batch") as tepoch:
+            for batch_idx, (data, target) in enumerate(tepoch):
+                loss = 0
+                tepoch.set_description(f"Test  Epoch {epoch}")
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                loss = loss_function(output, target)  # sum up batch loss
+                l1 = 0
+                if lambda_l1:
+                    for p in model.parameters():
+                        l1 = l1 + p.abs().sum()
+                loss = loss + (lambda_l1 * l1)
+                average_epoch_loss += loss.item()
+                correct_predictions = sum(output.argmax(dim = 1) == target)  # get the index of the max log-probability
+                correct_predictions_epoch += correct_predictions
+                tepoch.set_postfix(loss = round(loss.item(),5), accuracy = format(100 *correct_predictions.item()/data.shape[0], '.4f'))
+    return average_epoch_loss/batch_idx,correct_predictions_epoch/len(test_loader.dataset)
