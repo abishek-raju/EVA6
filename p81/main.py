528d2af389caa3cd888443b92f33d3ab0b46d91e
rampfire <abishek.ur@gmail.com>
2021-07-02 22:34:43 +0530

    modified main

--- p81/main.py
+++ p81/main.py
@@ -88,7 +88,7 @@ def main(config_json):
                                                   config_json["device"],
                                                   model)
 
-    return model,train_loader
+    return model,metric_log
 
 
 
