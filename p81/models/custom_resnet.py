9e1643fd8ae90f064b4b48f13909db8a71715cf0
rampfire <abishek.ur@gmail.com>
2021-07-08 16:41:25 +0530

    added custom resnet

--- p81/models/custom_resnet.py
+++ p81/models/custom_resnet.py
@@ -11,6 +11,34 @@ def normalization_technique(normalization,in_channels):
         return nn.BatchNorm2d(in_channels)
 
 
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(1,planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(1,planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(1,self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
 class Custom_Resnet(nn.Module):
     def __init__(self ,norm_type : "BN/LN/GN" = "BN"):
         super(Custom_Resnet, self).__init__()
