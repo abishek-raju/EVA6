78772b88c82abf1438a40dc853a8440eea397411
rampfire <abishek.ur@gmail.com>
2021-07-08 17:17:59 +0530

    added custom resnet

--- p81/models/custom_resnet.py
+++ p81/models/custom_resnet.py
@@ -18,10 +18,10 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.GroupNorm(1,planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.GroupNorm(1,planes)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
