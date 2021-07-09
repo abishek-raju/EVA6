c413f4adc1e4b0f22e1d514a3b11f45b3b5e0f94
rampfire <abishek.ur@gmail.com>
2021-07-09 23:54:59 +0530

    modified model.py

--- p81/models/custom_resnet.py
+++ p81/models/custom_resnet.py
@@ -1,53 +1,82 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+def normalization_technique(normalization,in_channels):
+    if normalization == "GN":
+        return nn.GroupNorm(2,in_channels)
+    elif normalization == "LN":
+        return nn.GroupNorm(1,in_channels)
+    else:
+        return nn.BatchNorm2d(in_channels)
 
 class Custom_Resnet(nn.Module):
-
-
-    def ResBlock(self, in_features, out_features, pading=1):
-      # convolution
-      layers = []
-      layers = [nn.Conv2d(in_features, out_features, 3, padding=pading, bias=False),nn.BatchNorm2d(out_features), nn.ReLU(),
-                nn.Conv2d(out_features, out_features, 3, padding=pading, bias=False),nn.BatchNorm2d(out_features), nn.ReLU()]
-      return nn.Sequential(*layers)
-
-    def max_pool_block(self, in_features, out_features, pading=1):
-        layers = []
-        layers = [nn.Conv2d(in_features, out_features, 3, padding=pading, bias=False), nn.MaxPool2d(2,2), nn.BatchNorm2d(out_features), nn.ReLU()]
-        return nn.Sequential(*layers)
-
-
-    def __init__(self):
+    def __init__(self ,norm_type : "BN/LN/GN"):
         super(Custom_Resnet, self).__init__()
-        self.convblock0 = nn.Sequential(nn.Conv2d(3,64, 3, padding=1, bias=False),nn.BatchNorm2d(64), nn.ReLU()) #38
-        self.pool1 = self.max_pool_block(64,128)  #19
-        self.convblock1 = self.ResBlock(128,128) #19
-        self.pool2 = self.max_pool_block(128,256)  #9   
-        self.pool3 = self.max_pool_block(256,512)  #4             
-        self.convblock2 = self.ResBlock(512,512) #4
-        
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,8),
+            nn.Dropout(0.01)
+        )
+        #28/26/3
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,8),
+            nn.Dropout(0.01)
+        )
+        #26/24/5
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,16),
+            nn.Dropout(0.01)
+        )
+        #24/22/7
 
-        # self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=4)) # output_size = 1
-        self.max_pool = nn.MaxPool2d(4,4)
-        self.fc1 = nn.Linear(512, 10)
-
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=18, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,18),
+            nn.Dropout(0.01)
+        )
+        #22/20/9
+        self.pool = nn.MaxPool2d(2, 2)
+        #20/10/10
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=18, out_channels=14, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,14),
+            nn.Dropout(0.01)
+        )
+        #10/8/14
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=14, out_channels=14, kernel_size=3),
+            nn.ReLU(),
+            normalization_technique(norm_type,14),
+            nn.Dropout(0.01)
+        )
+        #8/6/18
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=14, out_channels=10, kernel_size=1),
+            nn.ReLU(),
+            normalization_technique(norm_type,10),
+            nn.Dropout(0.01)
+        )
+        #6/6/18
+        self.gap = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1)
+        )
+    
     def forward(self, x):
-        x = self.convblock0(x)
-        x = self.pool1(x)
-        x1 = self.convblock1(x)
-        x = x+ x1
-        x = self.pool2(x)
-        x = self.pool3(x)
-        x2 = self.convblock2(x)
-        x = x + x2
-
-        
-        # print(x.shape)
-        # x = self.gap(x)
-        x = self.max_pool(x)
-        # print(x.shape)
-        x = x.view(-1,512)
-        x = self.fc1(x)
-        x = x.view(-1,10)
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x = self.convblock3(x)
+        x = self.convblock4(x)
+        x = self.pool(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.gap(x)
+        x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
