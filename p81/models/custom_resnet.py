0bdbf038966632b92768336b213383e40234c362
rampfire <abishek.ur@gmail.com>
2021-07-10 00:01:21 +0530

    modified model.py

--- p81/models/custom_resnet.py
+++ p81/models/custom_resnet.py
@@ -1,82 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 16:49:12 2021
+
+@author: rampfire
+"""
+
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def normalization_technique(normalization,in_channels):
-    if normalization == "GN":
-        return nn.GroupNorm(2,in_channels)
-    elif normalization == "LN":
-        return nn.GroupNorm(1,in_channels)
-    else:
-        return nn.BatchNorm2d(in_channels)
 
-class Custom_Resnet(nn.Module):
-    def __init__(self ,norm_type : "BN/LN/GN" = "BN"):
-        super(Custom_Resnet, self).__init__()
-        self.convblock1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,8),
-            nn.Dropout(0.01)
-        )
-        #28/26/3
-        self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,8),
-            nn.Dropout(0.01)
-        )
-        #26/24/5
-        self.convblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,16),
-            nn.Dropout(0.01)
-        )
-        #24/22/7
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=18, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,18),
-            nn.Dropout(0.01)
-        )
-        #22/20/9
-        self.pool = nn.MaxPool2d(2, 2)
-        #20/10/10
-        self.convblock5 = nn.Sequential(
-            nn.Conv2d(in_channels=18, out_channels=14, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,14),
-            nn.Dropout(0.01)
-        )
-        #10/8/14
-        self.convblock6 = nn.Sequential(
-            nn.Conv2d(in_channels=14, out_channels=14, kernel_size=3),
-            nn.ReLU(),
-            normalization_technique(norm_type,14),
-            nn.Dropout(0.01)
-        )
-        #8/6/18
-        self.convblock7 = nn.Sequential(
-            nn.Conv2d(in_channels=14, out_channels=10, kernel_size=1),
-            nn.ReLU(),
-            normalization_technique(norm_type,10),
-            nn.Dropout(0.01)
-        )
-        #6/6/18
-        self.gap = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1)
-        )
-    
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(1,planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.GroupNorm(1,planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(1,self.expansion*planes)
+            )
+
     def forward(self, x):
-        x = self.convblock1(x)
-        x = self.convblock2(x)
-        x = self.convblock3(x)
-        x = self.convblock4(x)
-        x = self.pool(x)
-        x = self.convblock5(x)
-        x = self.convblock6(x)
-        x = self.convblock7(x)
-        x = self.gap(x)
-        x = x.view(-1, 10)
-        return F.log_softmax(x, dim=-1)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.GroupNorm(1,64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def Custom_Resnet():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def test():
+    net = Custom_Resnet()
+    y = net(torch.randn(1, 3, 32, 32))
+    print(y.size())
+
+# test()
