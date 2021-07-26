102f755f3aac4c0b76b543be75234309cb8a88ba
rampfire <abishek.ur@gmail.com>
2021-07-26 16:36:47 +0530

    resnet with 200 classes

--- p81/utils/transformations.py
+++ p81/utils/transformations.py
@@ -14,9 +14,9 @@ def train_transform_list(mean,std_dev):
         [   
             A.Normalize(mean=mean, std=std_dev),
             A.Rotate(limit = (-5,5),always_apply = True),
-#            A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.50, rotate_limit=(-7,7), p=.75),
-#            A.CoarseDropout (max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=mean,p = 0.75),
-#            A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=mean, always_apply=True, p=0.5),
+            A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.50, rotate_limit=(-7,7), p=.75),
+            A.CoarseDropout (max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=mean,p = 0.75),
+            A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=mean, always_apply=True, p=0.5),
             A.HorizontalFlip(p=0.75),
             A.RandomCrop(32,32),
             ToTensorV2()
